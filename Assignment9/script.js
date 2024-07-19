a0b81ab56ba95d6992d94fd031adb4c5d271a30d
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2024-07-19 09:36:14 -0400

    Update script.js

--- Assignment9/script.js
+++ Assignment9/script.js
@@ -63,3 +63,69 @@ Student.serviceTaxRate = 15.0;
 const student2 = new Student(2, 'Jane Smith', 'ASP.NET');
 student2.payment(1000);
 student2.print();
+
+
+// 2] 
+
+class Course {
+  constructor(name, duration, fee) {
+    this.name = name;
+    this.duration = duration;
+    this.fee = fee;
+  }
+
+  print() {
+    document.write(`<b>Name:</b> ${this.name}<br>`);
+    document.write(`<b>Duration:</b> ${this.duration}<br>`);
+    document.write(`<b>Fee:</b> ${this.fee.toFixed(2)}<br><br>`);
+  }
+
+  getTotalFee() {
+    return this.fee;
+  }
+}
+
+class PartTimeCourse extends Course {
+  constructor(name, duration, fee, timing) {
+    super(name, duration, fee);
+    this.timing = timing;
+  }
+
+  print() {
+    super.print();
+    document.write(`<b>Timing:</b> ${this.timing}<br>`);
+  }
+
+  getTotalFee() {
+    return this.fee * 0.90;
+  }
+}
+
+class OnsiteCourse extends Course {
+  constructor(name, duration, fee, companyName, numCandidates) {
+    super(name, duration, fee);
+    this.companyName = companyName;
+    this.numCandidates = numCandidates;
+  }
+
+  print() {
+    super.print();
+    document.write(`<b>Company Name:</b> ${this.companyName}<br>`);
+    document.write(`<b>Number of Candidates:</b> ${this.numCandidates}<br>`);
+  }
+
+  getTotalFee() {
+    return this.fee * 1.10;
+  }
+}
+
+const partTimeCourse = new PartTimeCourse('Web Development', '6 months', 1000, 'Evenings');
+partTimeCourse.print();
+document.write(`<b>Total Fee:</b> $${partTimeCourse.getTotalFee().toFixed(2)} <br>`);
+
+const onsiteCourse = new OnsiteCourse('Data Science', '3 months', 1500, 'Tech Corp', 20);
+onsiteCourse.print();
+document.write(`<b>Total Fee:</b> $${onsiteCourse.getTotalFee().toFixed(2)}<br>`);
+
+document.write("----------------------------------------------------<br><br>");
+
